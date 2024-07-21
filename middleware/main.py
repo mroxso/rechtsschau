aab45713fa0a3f767fa73aac84079d396f6d58e6
mroxso <24775431+mroxso@users.noreply.github.com>
2024-07-21 19:25:25 +0200

    add middleware

---
+++ middleware/main.py
@@ -0,0 +1,35 @@
+# basic flask app
+from flask import Flask, request, jsonify
+import psycopg2
+from psycopg2 import sql
+import os
+
+
+# PostgreSQL Configuration
+db_config = {
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'database': os.getenv('DB_NAME', 'your_database'),
+    'user': os.getenv('DB_USER', 'your_username'),
+    'password': os.getenv('DB_PASS', 'your_password')
+}
+
+conn = psycopg2.connect(**db_config)
+cur = conn.cursor()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/api', methods=['GET'])
+def getapi():
+    get_query = sql.SQL('''SELECT * FROM rss_feed;''')
+    cur.execute(get_query)
+    results = cur.fetchall()
+    print(results)
+    return jsonify(results)
+    # return results
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
